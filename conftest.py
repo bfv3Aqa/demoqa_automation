0e53e289e4937b4708497d0982550b97d9f6e28c
root <root@localhost.localdomain>
2023-02-19 13:47:08 -0500

    updated conftest.py

--- conftest.py
+++ conftest.py
@@ -11,9 +11,11 @@ desiredCapabilities = {
 
 @pytest.fixture(scope="function")
 def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
     driver = webdriver.Remote(command_executor='http://192.168.1.55:4444/wd/hub',
                               desired_capabilities=desiredCapabilities)
-    driver.maximize_window()
+    # driver.maximize_window()
     yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f"Screenshot {datetime.now()}", attachment_type=allure.attachment_type.PNG)
