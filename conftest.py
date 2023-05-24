1b1cd6d2d8b810411c9d661d024f774c9dc7da9d
Andrei Zrelov <qa.zrelov.aa@gmail.com>
2023-05-24 13:34:48 +0400

    Added poetry + pytest addoptions

--- conftest.py
+++ conftest.py
@@ -1,25 +1,30 @@
 from datetime import datetime
 
-import os
 import allure
 import pytest
 from selenium import webdriver
 
-desiredCapabilities = {
-    "browserName": "chrome"
-}
 
-host = os.environ.get("MAIN_HOST")
-port = os.environ.get("MAIN_PORT")
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--executor", action="store", default="localhost")
+    parser.addoption("--bro_version", action="store", default="")
 
 
 @pytest.fixture(scope="function")
-def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Remote(command_executor=f'http://{host}:{port}/wd/hub',
-                              desired_capabilities=desiredCapabilities)
-    # driver.maximize_window()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    bro_version = request.config.getoption("--bro_version")
+    executor = request.config.getoption("--executor")
+
+    capabilities = {
+        "browserName": browser,
+        "browserVersion": bro_version,
+    }
+
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub", desired_capabilities=capabilities)
+
     yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f"Screenshot {datetime.now()}", attachment_type=allure.attachment_type.PNG)
